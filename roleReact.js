5d9105fd7cdf2e31edc008428c905ae143f7356e
HISPY-coder <60690110+HISPY-coder@users.noreply.github.com>
2020-02-05 16:18:47 +0700

    123

---
+++ roleReact.js
@@ -0,0 +1,18 @@
+//Settings!
+const yourID = "324462022316261378"; //Instructions on how to get this: https://redd.it/40zgse
+const setupCMD = "!createrolemessage"
+let initialMessage = `:flag_ru: : Хотите найти русскоговорящих собеседников? Нажмите на флажок РФ внизу, чтобы продолжить.
+:globe_with_meridians: : Want to keep The English Channel? Press the reaction below.
+
+* Нажмите кнопку реакции еще раз, чтобы отменить выбор языка.
+* Реакция не проявляется? нажмите CTRL+R
+* Реакция все еще не проявляется? откройте Настройки пользователя > включить " показать реакцию эмодзи на сообщение"
+Примечание: Вы можете выбрать только 2 роли
+
+* Press the reaction again to deselect language
+* Reaction not showing? press CTRL+R
+* Reaction still not showing? open user settings > Enable  "show emoji reaction on message"
+NOTE: You can only select 2 roles`;
+const roles = ["RU", "EN"];
+const reactions = [":flag_ru:", ":globe_with_meridians:🖌"];
+const botToken = "Njc0MjcwNzM5NzcxMTYyNjcw.Xjp61w.43Sk70JcXpv8-xrOm9hramMPdFY"; /*You'll have to set this yourself; read more
